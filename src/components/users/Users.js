7ea95e0922ad67486b7c09eefb1755e8c8541e5f
tarang369 <tarang369666@gmail.com>
2019-10-08 17:04:08 +0530

    first commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,29 @@
+import React from "react";
+import UserItems from "./UserItem";
+import Spinner from "../layouts/Spinner";
+import PropTypes from "prop-types";
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItems key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+Users.prototype = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
+
+export default Users;
