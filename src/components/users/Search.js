c450edca0ec847c38554e044a3569546f6186241
tarang369 <tarang369666@gmail.com>
2019-10-10 11:32:28 +0530

    refactoing search with hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,36 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-export class Search extends Component {
-  state = {
-    text: ""
-  };
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
+const Search =( {searchUsers,showClear,clearUsers,setAlert})=> {
+const [text,setText]= useState('');
 
-  onChange = e => this.setState({ text: e.target.value });
-  onSubmit = e => {
+  const onChange = e => setText(e.target.value );
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert(` Please enter something`, "light");
+    if (text === "") {
+      setAlert(` Please enter something`, "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
-  render() {
-    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="form">
+        <form onSubmit={onSubmit} className="form">
           <input
             type="text"
             placeholder="Search Users..."
-            value={this.state.text}
-            onChange={this.onChange}
+            value={text}
+            onChange={onChange}
           />
           <input
             type="submit"
@@ -45,7 +36,15 @@ export class Search extends Component {
         )}
       </div>
     );
-  }
+  
 }
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
+
 
 export default Search;
